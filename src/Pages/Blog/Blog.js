f7e7a1187fdf59c6e8fd71e6d7acc173d9eb4ef7
najmul-rusu <nhrusu@gmail.com>
2022-05-08 13:41:02 +0600

    Q & A added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,59 +1,109 @@
-import React from 'react';
-import './Blog.css'
+import React from "react";
+import "./Blog.css";
 
 const Blog = () => {
-    return (
-        <div className='qna'>
-            <h2 className='qna-titel'>Question and Answer</h2>
-            <h3>Why are you using firebase? What other options do you have to implement authentication?</h3>
-            <p>Firebase manages all the data in the database in real time. Thus, exchanging data from database is easy and fast. So, if you want to create mobile apps like live streaming, chat messaging, etc., you can use Firebase.</p>
-            <p>One of the most important aspects of website authentication is the focus on user and human-to-computer interaction. As a result, user authentication is important to understand when creating or improving your website's login system.
-
-                Whether you want to increase your internal security, increase customer acquisition, or provide a better user experience for people exploring your site, it's important to know how it fits into the user authentication equation.
-            </p>
-
-            <h3>What other services does firebase provide other than authentication?</h3>
-
-            <p>The goal of every developer is to create great apps. If you're a developer, we're here to explore some of Firebase's best alternatives to take your mobile app development to new heights.
-
-
-                Firebase continues to innovate and dominate the backend as a service (BaaS) market. It has grown so much that it has become an integral part of Google's mobile strategy. I've been a passionate Firebase user in the past, but my concern about its complexity and cost led me to explore other cloud options.
-            </p>
-
-            <h3>Difference between javascript and node js</h3>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Javascript</th>
-                            <th>Node js</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Javascript is a programming language that is used for writing scripts on the website.</td>
-                            <td>NodeJS is a Javascript runtime environment.</td>
-                        </tr>
-                        <tr>
-                            <td>Javascript can only be run in the browsers.</td>
-                            <td>We can run Javascript outside the browser with the help of NodeJS.</td>
-                        </tr>
-                        <tr>
-                            <td>Javascript is capable enough to add HTML and play with the DOM.</td>
-                            <td>Nodejs does not have capability to add HTML tags.</td>
-                        </tr>
-                        <tr>
-                            <td>Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox.</td>
-                            <td>V8 is the Javascript engine inside of node.js that parses and runs Javascript.</td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-
-                    </tfoot>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="qna">
+      <h2 className="qna-title">Question and Answer</h2>
+      {/* Question and Answer - 01  */}
+      <h4 className="mt-5">Difference between javascript and node js</h4>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Javascript</th>
+              <th>Node js</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                Javascript is a programming language that is used for writing
+                scripts on the website.
+              </td>
+              <td>NodeJS is a Javascript runtime environment.</td>
+            </tr>
+            <tr>
+              <td>Javascript can only be run in the browsers.</td>
+              <td>
+                We can run Javascript outside the browser with the help of
+                NodeJS.
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Javascript is capable enough to add HTML and play with the DOM.
+              </td>
+              <td>Nodejs does not have capability to add HTML tags.</td>
+            </tr>
+            <tr>
+              <td>
+                Javascript can run in any browser engine as like JS core in
+                safari and Spidermonkey in Firefox.
+              </td>
+              <td>
+                V8 is the Javascript engine inside of node.js that parses and
+                runs Javascript.
+              </td>
+            </tr>
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+      </div>
+      {/* Question and Answer - 02*/}
+      <h4 className="mt-5">When should you use nodejs and when should you use mongodb?</h4>
+      <p>
+        Nodejs : Node.js is an interpreter or runtime / running environment for
+        JavaScript. Built on Chrome's V8 JavaScript engine. Responsibilities
+        especially make your application effective.
+      </p>
+      <p>
+        MongoDB : A No-SQL database for storing data. When high availability of
+        data is required with automatic, fast, and instant data recovery.
+      </p>
+      {/* Question and Answer - 03*/}
+      <h4 className="mt-5">Differences between SQL and NoSQL databases.</h4>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>SQL</th>
+              <th>NoSQL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>These databases have fixed or static or predefined schema</td>
+              <td>They have dynamic schema</td>
+            </tr>
+            <tr>
+              <td>
+                These databases are not suited for hierarchical data storage.
+              </td>
+              <td>
+                These databases are best suited for hierarchical data storage.
+              </td>
+            </tr>
+            <tr>
+              <td>These databases are best suited for complex queries</td>
+              <td>These databases are not so good for complex queries</td>
+            </tr>
+            <tr>
+              <td>Vertically Scalable</td>
+              <td>Horizontally scalable</td>
+            </tr>
+            <tr>
+              <td>Follows ACID property</td>
+              <td>
+                Follows CAP(consistency, availability, partition tolerance)
+              </td>
+            </tr>
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
