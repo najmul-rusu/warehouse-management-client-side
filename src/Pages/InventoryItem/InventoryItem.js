21e875e248d65fa2f1db15b5d1538bb545c4730a
najmul-rusu <nhrusu@gmail.com>
2022-05-20 04:54:35 +0600

    update stock of inventory item

--- src/Pages/InventoryItem/InventoryItem.js
+++ src/Pages/InventoryItem/InventoryItem.js
@@ -1,40 +1,99 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const InventoryItem = () => {
+
   const { inventoryId } = useParams();
-  const [inventory, setInventory] = useState({});
-  const {picture, name, quantity, price, supplier_name, description} = inventory;
+const navigate = useNavigate();
+const [inventory, setInventory] = useState({});
+const [res, setRes] = useState({});
+const [reStock, setReStock] = useState(false);
 
-  useEffect(() => {
+useEffect(() => {
     const url = `http://localhost:5000/inventory/${inventoryId}`;
-
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => setInventory(data));
-  }, []);
+        .then(res => res.json())
+        .then(data => setInventory(data));
+}, [inventoryId, res])
+const { name, picture, description, price, quantity, supplier_name } = inventory;
 
-  return (
-    <div className="container d-flex my-5 border border-dark py-5">
-      <div className="">
-        <img className="w-75" src={picture} alt="iPhone" />
+//? Handle Delivery operation
+const handleDeliver = () => {
+    if (quantity > 0) {
+        const newQuantity = {
+            quantity: (quantity - 1)
+        };
+
+        const url = `http://localhost:5000/inventory/${inventoryId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newQuantity),
+        })
+            .then(res => res.json())
+            .then(data => setRes(data));
+    }
+}
+
+//? Handle restock submittion
+const handleForm = (event) => {
+    event.preventDefault();
+    const amount = parseInt(event.target.amount.value);
+    const newQuantity = {
+        quantity: (quantity + amount),
+    }
+    console.log(newQuantity)
+    const url = `http://localhost:5000/inventory/${inventoryId}`;
+    fetch(url, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(newQuantity)
+    })
+        .then(res => res.json())
+        .then(data => setRes(data))
+}
+return (
+    <div className='container '>
+        <div className="card border-0 my-4" >
+            <div className="row g-0 shadow  my-4 bg-light rounded text-start">
+                <div className="col-md-4">
+                    <img src={picture} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8 P-0">
+                    <div className="card-body p-2 px-4">
+                        <h5 className="card-title">{name}</h5>
+                        <p><b>Price: </b>{price}</p>
+                        <p className="card-text">{description}</p>
+                        <p><b>Quantity: </b>{quantity}</p>
+                        <p><b>SuplierName: </b>{supplier_name}</p>
+                        <div className='d-grid mt-2 w-50'>
+                            <button className='m-2 btn btn-primary' onClick={() => setReStock(!reStock)}>Restock</button>
+
+                            <button className='m-2 btn btn-danger' onClick={handleDeliver}>Delivered</button>
+                            <button onClick={() => navigate('/manage-items')} className='m-2 btn btn-success'>Manage Invenotires</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {reStock && (
+                            <div className="inventory_restock">
+                                <form onSubmit={handleForm}>
+                                    <div className="inventory_restock_input_field">
+                                        <input className="inventory_restock_input_field" type="number" name="amount" placeholder="Enter Amount" />
+                                    </div>
+                                    <div className="inventory_restock_input_field">
+                                        <input className="inventory_restock_input_field" type="submit" value="Submit" />
+                                    </div>
+                                </form>
+                            </div>
+                        )}
         </div>
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">Quantity: {quantity}</p>
-          <p className="card-text">Price: {price}</p>
-          <p className="card-text text-muted">
-            Supplier name: {supplier_name}
-          </p>
-          <p className="card-text pb-4">{description}</p>
-      </div>
-      <div className="d-grid gap-3 col-3 mx-auto">
-  <Button className="btn btn-secondary" type="button">Delivery</Button>
-  <Button className="btn btn-secondary" type="button">Restock</Button>
-</div>
     </div>
-  );
+);
 };
 
 export default InventoryItem;
